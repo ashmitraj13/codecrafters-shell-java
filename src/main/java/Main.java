9331ff2e5a904607436cad582f04bef32c6435ff
Ashmit Raj <ashmitraj@Ashmits-MacBook-Pro.local>
2026-06-18 13:46:54 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
         while (true) {
             System.out.print("$ ");
             System.out.flush();
@@ -12,12 +11,22 @@ public class Main {
                 break;
             }
 
-            String st = sc.nextLine();
-            if (st.equals("exit")) {
+            String input = sc.nextLine();
+            String trimmed = input.stripLeading();
+            if (trimmed.equals("exit")) {
                 break;
             }
 
-            System.out.println(st + ": command not found");
+            String[] parts = trimmed.split("\\s+", 2);
+            if (parts.length > 0 && parts[0].equals("echo")) {
+                if (parts.length > 1) {
+                    System.out.println(parts[1]);
+                } else {
+                    System.out.println();
+                }
+            } else {
+                System.out.println(input + ": command not found");
+            }
         }
     }
 }
